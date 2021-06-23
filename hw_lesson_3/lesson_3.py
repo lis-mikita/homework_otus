8a3c486e38f7cc8539a5082047b9e12632dca7da
lis_nikita <fox.nn.aa@gmail.com>
2021-06-23 18:44:48 +0300

    Change square_of_number, add keyword for degree

--- hw_lesson_3/lesson_3.py
+++ hw_lesson_3/lesson_3.py
@@ -1,9 +1,10 @@
 # Homework of lesson 3
 
-def square_of_numbers(*args):
+def square_of_numbers(keyword, *args):
     """
     :param args: N integer
+    :param keyword: Choice of degree of N integer
     :return: list of square N integer
     """
     son_list = []
-    return [v * v for v in args]
+    return [v ** keyword for v in args]
