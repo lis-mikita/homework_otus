2035bad313a43ca1eb4f7ae602010dfc4e6e58ee
lis_nikita <fox.nn.aa@gmail.com>
2021-06-23 18:39:19 +0300

    Change def square_of_number

--- hw_lesson_3/lesson_3.py
+++ hw_lesson_3/lesson_3.py
@@ -1,4 +1,9 @@
 # Homework of lesson 3
 
 def square_of_numbers(*args):
-    pass
+    """
+    :param args: N integer
+    :return: list of square N integer
+    """
+    son_list = []
+    return [v * v for v in args]
