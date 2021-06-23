60b29a95217136e8c287d78f2acaa46f684074e7
lis_nikita <fox.nn.aa@gmail.com>
2021-06-23 18:51:06 +0300

    Add def int_conversion

--- hw_lesson_3/lesson_3.py
+++ hw_lesson_3/lesson_3.py
@@ -8,3 +8,21 @@ def square_of_numbers(keyword, *args):
     """
     son_list = []
     return [v ** keyword for v in args]
+
+def int_conversion(list_int, keyword):
+    """
+    :param list_int:
+    :param keyword:
+    :return:
+    """
+    ic_list = list_int
+    if keyword == "even":
+        pass
+    elif keyword == "odd":
+        pass
+    elif keyword == "prime":
+        pass
+    else:
+        pass
+    return ic_list
+# TODO add feature for def int_conversion
